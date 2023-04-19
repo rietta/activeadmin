4675794a235a6e5a9d22e2c14981e5532b3ba880
Frank Rietta <frank@rietta.com>
2023-04-18 22:22:15 -0400

    Revert last tweak

--- lib/active_admin/views/pages/page.rb
+++ lib/active_admin/views/pages/page.rb
@@ -21,10 +21,8 @@ module ActiveAdmin
         def title
           if page_presenter[:title]
             render_or_call_method_or_proc_on self, page_presenter[:title]
-          elsif active_admin_config.respond_to?(:name)
+          else
             active_admin_config.name
-          else 
-            'Untitled'
           end
         end
       end
